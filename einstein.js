5989f5b0a766466deda73d03b14d7330a0c1172c
KristyKostyor <ms.kostyor@gmail.com>
2023-04-24 14:54:17 +0300

    Init

---
+++ einstein.js
@@ -0,0 +1,3 @@
+const catWeight = 5;
+const speedLight = 3e8;
+console.log(catWeight* Math.pow(speedLight,2));
---
+++ mod2-2.js
@@ -0,0 +1,24 @@
+const userName = prompt("Здравствуйте, как Вас зовут?");
+    alert(`Добро пожаловать, ${userName}`);
+const productName = prompt("Наименование товара");
+    alert (`Вы выбрали ${productName}`);
+const category = prompt("Назовите категорию товара");
+    alert(`Категория: ${category}`);
+const amount = prompt('Введите количество товара');
+    alert(`Вы выбрали ${amount}`);
+
+let price;
+while(isNaN(price)){
+    price = +prompt("Введите цену товара");
+
+
+if(isNaN(price)) {
+    alert("Введите цифры");
+}
+else {
+    price == "";
+    alert(`Стоимость товара ${price}$`)
+};
+};
+alert(`${userName}, Вы выбрали ${productName}, категория: ${category}, итоговая стоимость: ${price * amount}`);
+alert(`До свидания, ${userName}`);
